aedb73e21d651c42ad153fdab2c4f62b34bc1e4d
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-06-01 17:57:52 -0400

    feat: adding css

--- product-preview-card-component-main/src/App.tsx
+++ product-preview-card-component-main/src/App.tsx
@@ -4,22 +4,22 @@ import cartIcon from "./assets/icon-cart.svg";
 const App = () => {
 	return (
 		<>
-			<article>
-				<aside>
+			<article className="product">
+				<aside className="product__image">
 					<img src={mobileImage} alt="Mobile Image" />
 				</aside>
-				<main>
-					<span className="product-type">Perfume</span>
-					<h1 className="product-title">Gabrielle Essence Eau De Parfum</h1>
-					<p className="product-description">
+				<main className="product__info">
+					<span className="product__type">Perfume</span>
+					<h1 className="product__title">Gabrielle Essence Eau De Parfum</h1>
+					<p className="product__description">
 						A floral, solar and voluptuous interpretation composed by Olivier
 						Polge, Perfumer-Creator for the House of CHANEL.
 					</p>
-					<div className="product-price">
-						<span className="product-current-price">$149.99</span>
-						<span className="product-old-price">$169.99</span>
+					<div className="product__price">
+						<span className="product__price-current">$149.99</span>
+						<span className="product__price-old">$169.99</span>
 					</div>
-					<button className="product-buy">
+					<button className="product__buy">
 						<img src={cartIcon} alt="Add to Cart" />
 						Add to Cart
 					</button>
--- product-preview-card-component-main/src/index.tsx
+++ product-preview-card-component-main/src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
+
+/* Oportunidad para mujeres en Chile
+Increíble oportunidad para mujeres en tecnología!
+Si vives en Chile y estás buscando tu primera experiencia laboral como Desarrolladora de Software, este post es para ti!
+Estamos buscando al mejor talento femenino para acelerar sus habilidades como Fullstack Developers y formar parte de una de las compañías más importantes de Chile.
+Para participar ingresa al siguiente enlace () y sigue los pasos
+Si conoces a alguien, por favor compartir!
+*/
