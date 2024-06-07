04514e254b3fc1cac09ca27a34cd39b7cbeb9f8a
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-06-06 21:43:57 -0400

    feat: style changes

--- social-media-dashboard-with-theme-switcher-master/src/Social.tsx
+++ social-media-dashboard-with-theme-switcher-master/src/Social.tsx
@@ -4,8 +4,10 @@ const rrss = () => {
 	return (
 		<section className="rrss">
 			<header className="rrss__header">
-				<h1 className="rrss__title">Social Media Dashboard</h1>
-				<span className="rrss__total-followers">Total Followers: 23,004</span>
+				<section className="rrss__total">
+					<h1 className="rrss__title">Social Media Dashboard</h1>
+					<span className="rrss__total-followers">Total Followers: 23,004</span>
+				</section>
 				<div className="switch">Dark/Light Mode</div>
 			</header>
 			<main className="rrss__main">
