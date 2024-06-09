26ca024a667b25a987c51ee22267825b4e54fdae
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-06-08 20:05:05 -0400

    style: some modifications to mobile

--- social-media-dashboard-with-theme-switcher-master/src/Social.tsx
+++ social-media-dashboard-with-theme-switcher-master/src/Social.tsx
@@ -8,7 +8,10 @@ const rrss = () => {
 					<h1 className="rrss__title">Social Media Dashboard</h1>
 					<span className="rrss__total-followers">Total Followers: 23,004</span>
 				</section>
-				<div className="switch">Dark/Light Mode</div>
+				<div className="switch">
+					<input type="checkbox switch checked" />
+					Dark/Light Mode
+				</div>
 			</header>
 			<main className="rrss__main">
 				<SocialCard />
