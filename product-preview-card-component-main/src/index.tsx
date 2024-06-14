7f4aa7fb8602eccdcaf6e664cff5544e483a635e
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-06-13 21:47:02 -0400

    adding info to starting project

--- product-preview-card-component-main/src/index.tsx
+++ product-preview-card-component-main/src/index.tsx
@@ -8,11 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 		<App />
 	</React.StrictMode>
 );
-
-/* Oportunidad para mujeres en Chile
-Increíble oportunidad para mujeres en tecnología!
-Si vives en Chile y estás buscando tu primera experiencia laboral como Desarrolladora de Software, este post es para ti!
-Estamos buscando al mejor talento femenino para acelerar sus habilidades como Fullstack Developers y formar parte de una de las compañías más importantes de Chile.
-Para participar ingresa al siguiente enlace () y sigue los pasos
-Si conoces a alguien, por favor compartir!
-*/
--- starting-project.md
+++ starting-project.md
@@ -5,3 +5,16 @@ npm create vite@latest
 # adding sass:
 
 npm install -g sass
+
+# local storage:
+
+npm install use-local-storage
+
+# adding icons:
+
+npm i react-icons
+https://react-icons.github.io/react-icons/
+
+# react router dom
+
+npm install react-router-dom
